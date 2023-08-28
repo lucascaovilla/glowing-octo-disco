6d4acfc5a888ec9ce84dc815c91c4b2316ed96d5
Lucas Grisa Caovilla <114552388+lucascaovilla@users.noreply.github.com>
2023-08-28 16:54:02 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
-  medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  name: "Lucas Caovilla",
+  title: "Software Developer",
+  email: "lucasgrisac@gmail.com",
+  gitHub: "https://github.com/lucascaovilla",
+  instagram: "",
+  linkedIn: "https://linkedin.com/in/lucasgcaovilla",
+  medium: "https://medium.com/@lucascaovilla",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
